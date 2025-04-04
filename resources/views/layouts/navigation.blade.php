25146f5db69776c5edbc5a657a1a1d36a8613cfc
Kholzt <99956928+Kholzt@users.noreply.github.com>
2025-04-04 10:21:04 +0700

    update sidebar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,9 +24,16 @@
             <div class="flex items-center ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex items-start px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                           <div class="text-end">
                             <div>{{ Auth::user()->name }}</div>
-
+                            <small class="text-xs text-end text-slate-400 relative inline-flex gap-1 items-center">
+                                <span class="relative flex size-2">
+                                    <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+                                    <span class="relative inline-flex size-2 rounded-full bg-green-500"></span>
+                                  </span>
+                                  {{auth()->user()->role}}</small>
+                           </div>
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 <aside class=" md:min-w-[300px] md:sticky md:left-0 min-w-full h-screen  fixed top-0   z-10 md:bg-gray-100 transition-all flex flex-col bg-slate-200" :class="{'left-[-100%]': ! sidebarOpen, 'left-0': sidebarOpen }">
+=======
+<aside class=" md:min-w-[300px] md:sticky md:left-0 min-w-full h-screen overflow-auto  fixed top-0 shadow-lg  z-10  transition-all  flex flex-col bg-white" :class="{'left-[-100%]': ! sidebarOpen, 'left-0': sidebarOpen }">
+>>>>>>> Stashed changes
     <div class="logo p-4 flex justify-between">
         <a href="{{ route('dashboard') }}">
             <img src="{{asset(auth()->user()->pengaturan()->logo_horizontal? "assets/logos/".auth()->user()->pengaturan()->logo_horizontal:"image/default-2.png" )}}" alt="" class="h-[40px]">
@@ -11,7 +15,7 @@
         </button>
     </div>
 
-    <div class=" px-4 ">
+    {{-- <div class=" px-4 ">
         <div class="flex gap-2 items-center bg-gray-200 w-full p-2 rounded-md">
             <img src="{{asset("assets/image/".auth()->user()->avatar)}}" alt="profile image" class="w-[50px] h-[50px] rounded-full bg-gray-200">
             <div class="">
@@ -19,8 +23,9 @@
                 <span class="text-sm text-slate-600">{{auth()->user()->role}}</span>
             </div>
         </div>
-    </div>
+    </div> --}}
 
+<<<<<<< Updated upstream
     <div class="flex px-4 py-4  flex-col flex-grow justify-between">
         <ul class=" ">
             <li>
@@ -62,6 +67,99 @@
             <li>
                 <!-- <span class="text-lg text-gray-600 block mb-2">Dashboard</span> -->
                 <a href="{{route("setting.index")}}" class="block   px-4 py-2 {{request()->is('c/*/setting') ? 'bg-[var(--primary)] text-white' : 'text-slate-700 md:hover:bg-white/80 '}} transition-all  rounded-md text-base flex items-center gap-2"><i class="fa w-[30px] fa-gear text-lg"></i> Pengaturan</a>
+=======
+    <div class="flex px-4 py-4 gap-2  flex-col flex-grow justify-between">
+        <ul class="">
+            <li class="fi-sidebar-item-label flex-1 my-2 truncate text-sm font-medium text-primary-600 dark:text-primary-400">
+                <!-- <span class="text-lg text-gray-600 block mb-2">Dashboard</span> -->
+                <a href="{{route("dashboard")}}" class="block   px-2 py-1 {{request()->is('c/*/dashboard') ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}} transition-all   rounded-md text-sm flex items-center gap-2"><i class="fa w-[30px] fa-border-all text-lg"></i> Dashboard</a>
+            </li>
+            {{-- <hr class="block mt-6 mb-2 border-slate-300"> --}}
+            <li class="fi-sidebar-item-label flex-1 my-2 truncate text-sm font-medium text-primary-600 dark:text-primary-400">
+                <!-- <span class="text-lg text-gray-600 block mb-2">Dashboard</span> -->
+                <a href="{{route("pengajuan-surat.index")}}" class="block   px-2 py-1 {{request()->is('c/*/pengajuan-surat') ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}} transition-all   rounded-md text-sm flex items-center gap-2"><i class="fa w-[30px] fa-envelope text-lg"></i> Surat Masuk</a>
+            </li>
+            <li class="fi-sidebar-item-label flex-1 my-2 truncate text-sm font-medium text-primary-600 dark:text-primary-400">
+                <!-- <span class="text-lg text-gray-600 block mb-2">Dashboard</span> -->
+                <a href="{{route("pengajuan-surat.index")}}" class="block   px-2 py-1 {{request()->is('c/*/pengajuan-surat') ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}} transition-all   rounded-md text-sm flex items-center gap-2"><i class="fa w-[30px] fa-envelope text-lg"></i> Surat Keluar</a>
+            </li>
+
+            {{-- <hr class="block mt-6 mb-2 border-slate-300"> --}}
+            <li class="mt-6 mb-6 cursor-pointer" x-data="{ isCollapse: true }">
+                <!-- Header -->
+                <div @click="isCollapse = !isCollapse" class="flex justify-between items-center text-gray-400 hover:text-gray-800 transition-all">
+                    <span class="text-sm font-medium ">Master Data</span>
+                    <i class="fa transition-transform duration-300" :class="{'fa-chevron-down': isCollapse, 'fa-chevron-up': !isCollapse}"></i>
+                </div>
+
+                <!-- Dropdown dengan animasi height -->
+                <ul class="overflow-hidden transition-all duration-300 ease-in-out "
+                    :class="isCollapse ? 'max-h-[500px] opacity-100 mt-4' : 'max-h-0 opacity-0'">
+
+                    <li>
+                        <a href="{{ route('rw.index') }}"
+                            class="flex  px-2 py-2 rounded-md text-sm  items-center gap-2  hover:bg-gray-100 transition-all font-medium {{request()->routeIs("rw.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}}">
+                            <i class="fa fa-circle {{request()->routeIs("rw.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-gray-400'}} text-[6px] w-6"></i> RT & RW
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('kartu-keluarga.index') }}"
+                            class="flex  px-2 py-2 rounded-md text-sm  items-center gap-2  hover:bg-gray-100 transition-all font-medium {{request()->routeIs("kartu-keluarga.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}}">
+                            <i class="fa fa-circle {{request()->routeIs("kartu-keluarga.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-gray-400'}} text-[6px] w-6"></i> Kartu Keluarga
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('users.index') }}"
+                            class="flex  px-2 py-2 rounded-md text-sm  items-center gap-2  hover:bg-gray-100 transition-all font-medium {{request()->routeIs("users.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}}">
+                            <i class="fa fa-circle {{request()->routeIs("users.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-gray-400'}} text-[6px] w-6"></i> Users
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('surat.index') }}"
+                            class="flex  px-2 py-2 rounded-md text-sm  items-center gap-2  hover:bg-gray-100 transition-all font-medium  {{request()->routeIs("surat.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}}">
+                            <i class="fa fa-circle {{request()->routeIs("surat.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-gray-400'}} text-[6px] w-6"></i> Surat
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('berita.index') }}"
+                            class="flex  px-2 py-2 rounded-md text-sm  items-center gap-2  hover:bg-gray-100 transition-all font-medium  {{request()->routeIs("berita.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}}">
+                            <i class="fa fa-circle {{request()->routeIs("berita.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-gray-400'}} text-[6px] w-6"></i> Berita
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('format-surat.index') }}"
+                            class="flex  px-2 py-2 rounded-md text-sm  items-center gap-2  hover:bg-gray-100 transition-all font-medium  {{request()->routeIs("format-surat.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}}">
+                            <i class="fa fa-circle {{request()->routeIs("format-surat.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-gray-400'}} text-[6px] w-6"></i> Format Surat
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="mt-6 mb-6 cursor-pointer" x-data="{ isCollapse: true }">
+                <!-- Header -->
+                <div @click="isCollapse = !isCollapse" class="flex justify-between items-center text-gray-400 hover:text-gray-800 transition-all">
+                    <span class="text-sm font-medium">Pengaturan</span>
+                    <i class="fa transition-transform duration-300" :class="{'fa-chevron-down': isCollapse, 'fa-chevron-up': !isCollapse}"></i>
+                </div>
+                <!-- Dropdown dengan animasi height -->
+                <ul class="overflow-hidden transition-all duration-300 ease-in-out mt-4"
+                    :class="isCollapse ? 'max-h-[500px] opacity-100' : 'max-h-0 opacity-0'">
+
+                    <li>
+                        <a href="{{ route('setting.index') }}"
+                            class="flex  px-2 py-2 rounded-md text-sm  items-center gap-2  hover:bg-gray-100 transition-all font-medium  {{request()->routeIs("setting.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}}">
+                            <i class="fa fa-circle text-gray-400 text-[6px] w-6"></i> Tampilan
+                        </a>
+                    </li>
+                    <li>
+
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type="submit" class=" flex text-start  px-2 py-2 rounded-md text-sm  items-center gap-2  hover:bg-gray-100 transition-all font-medium w-full "><i class="fa fa-circle text-gray-400 text-[6px] w-6"></i> Logout</button>
+
+                        </form>
+                    </li>
+                </ul>
+>>>>>>> Stashed changes
             </li>
         </ul>
         <ul>
