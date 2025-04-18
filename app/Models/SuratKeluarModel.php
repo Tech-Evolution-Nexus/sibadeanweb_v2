d692eee3a43e603f9c7f5350cc37aa694befa18c
Ryuushu <kzkzaj@gmail.com>
2025-04-18 18:33:24 +0700

    surat-keluar

---
+++ app/Models/SuratKeluarModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SuratKeluarModel extends Model
+{
+    use HasFactory;
+    protected $table = "surat_keluar";
+    protected $fillable = ["title", "nama_file", "exp_date"];
+}
