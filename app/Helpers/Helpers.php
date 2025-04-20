2d8f4e426f4ec1a7172c0697452216bd161bc9b2
Ryuushu <kzkzaj@gmail.com>
2025-04-20 14:01:46 +0700

    baru api

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -20,3 +20,22 @@ class Helpers
         return Carbon::parse($date)->translatedFormat('d F Y');
     }
 }
+class ResponseHelper
+{
+  
+    static function success($data = [], $message = 'Success', $code = 200)
+    {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+    static function error($message = 'Error', $code = 400)
+    {
+        return response()->json([
+            'status' => false,
+            'message' => $message,
+        ], $code);
+    }
+}
