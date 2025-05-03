270de7b3a349638195c94821bd3eff98478cccd7
chuzXII <kzkzaj@gmail.com>
2025-05-04 01:51:35 +0700

    ba

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,3 +1,4 @@
+
 <aside class=" md:min-w-[300px] md:sticky md:left-0 min-w-full h-screen overflow-auto   fixed top-0 border-r  z-10  transition-all  flex flex-col bg-white hide-scroll" :class="{'left-[-100%]': ! sidebarOpen, 'left-0': sidebarOpen }">
     <div class="logo p-4 flex justify-between">
         <a href="{{ route('dashboard') }}">
@@ -14,11 +15,11 @@
     {{-- <div class=" px-4 ">
         <div class="flex gap-2 items-center bg-gray-200 w-full p-2 rounded-md">
             <img src="{{asset("assets/image/".auth()->user()->avatar)}}" alt="profile image" class="w-[50px] h-[50px] rounded-full bg-gray-200">
-    <div class="">
-        <span class="text-sm block">{{auth()->user()->name}}</span>
-        <span class="text-sm text-slate-600">{{auth()->user()->role}}</span>
-    </div>
-    </div>
+            <div class="">
+                <span class="text-sm block">{{auth()->user()->name}}</span>
+                <span class="text-sm text-slate-600">{{auth()->user()->role}}</span>
+            </div>
+        </div>
     </div> --}}
 
 
@@ -32,20 +33,6 @@
             </li>
             {{-- <hr class="block mt-6 mb-2 border-slate-300"> --}}
             <li class="fi-sidebar-item-label flex-1 my-2 truncate text-sm font-medium text-primary-600 dark:text-primary-400" x-data="{ isCollapse: true }">
-                <div @click="isCollapse = !isCollapse" class="flex justify-between items-center text-gray-800 transition-all">
-                    <div class="   px-2 py-1  transition-all   rounded-md text-sm flex items-center gap-2"><i class="fa w-[30px] fa-envelope text-lg"></i> Surat Masuk</div>
-                    <i class="fa transition-transform duration-300" :class="{'fa-chevron-down': isCollapse, 'fa-chevron-up': !isCollapse}"></i>
-                </div>
-                <!-- Dropdown dengan animasi height -->
-                <ul class="overflow-hidden transition-all duration-300 ease-in-out px-4"
-                    :class="isCollapse ? 'max-h-[500px] opacity-100 mt-4' : 'max-h-0 opacity-0'">
-                    <li>
-                        <a href="{{ route('pengajuan-surat.index') }}"
-                            class="flex  px-2 py-2 rounded-md text-sm  items-center gap-2  hover:bg-gray-100 transition-all font-medium {{request()->routeIs("pengajuan-surat.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}}">
-                            <div class="">
-                                <i class="fa fa-circle {{request()->routeIs("pengajuan-surat.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-gray-400'}} text-[6px] w-6"></i> Menunggu Persetujuan
-                            </div>
-                            <span class=" items-center rounded-md bg-gray[--primary] px-2 py-1 text-xs font-medium text-[--primary] ring-1 ring-[--primary] ring-inset ms-auto {{Helpers::getCountPengajuan()->countMenungguPengajuan ? "inline-flex":"hidden"}}">{{Helpers::getCountPengajuan()->countMenungguPengajuan}}</span>
                     <div @click="isCollapse = !isCollapse" class="flex justify-between items-center text-gray-800 transition-all">
                         <div  class="   px-2 py-1  transition-all   rounded-md text-sm flex items-center gap-2"><i class="fa w-[30px] fa-envelope text-lg"></i> Surat Masuk</div>
                 <i class="fa transition-transform duration-300" :class="{'fa-chevron-down': isCollapse, 'fa-chevron-up': !isCollapse}"></i>
@@ -59,15 +46,6 @@
                         <i class="fa fa-circle {{request()->routeIs("pengajuan-surat.index") && (request()->query('status') == 'menunggu'||request()->query('status') == '') ? 'text-[var(--primary)] bg-gray-100' : 'text-gray-400'}} text-[6px] w-6"></i> Menunggu Persetujuan</div>
                         <span class=" items-center rounded-md bg-gray[--primary] px-2 py-1 text-xs font-medium text-[--primary] ring-1 ring-[--primary] ring-inset ms-auto {{Helpers::getCountPengajuan()->countMenungguPengajuan ? "inline-flex":"hidden"}}">{{Helpers::getCountPengajuan()->countMenungguPengajuan}}</span>
 
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('pengajuan-surat-rt.index') }}"
-                            class="flex  px-2 py-2 rounded-md text-sm  items-center gap-2  hover:bg-gray-100 transition-all font-medium {{request()->routeIs("pengajuan-surat-rt.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}}">
-                            <i class="fa fa-circle {{request()->routeIs("pengajuan-surat-rt.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-gray-400'}} text-[6px] w-6"></i> Disetujui Rt
-                        </a>
-                    </li>
-                </ul>
                     </a>
                 </li>
                 <li>
@@ -133,7 +111,7 @@
                     <li>
                         <a href="{{ route('lampiran.index') }}"
                             class="flex  px-2 py-2 rounded-md text-sm  items-center gap-2  hover:bg-gray-100 transition-all font-medium  {{request()->routeIs("lampiran.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-slate-700 md:hover:bg-gray-100'}}">
-                            <i class="fa fa-circle text-gray-400 text-[6px] w-6"></i>Lampiran
+                            <i class="fa fa-circle {{request()->routeIs("lampiran.index") ? 'text-[var(--primary)] bg-gray-100' : 'text-gray-400'}} text-[6px] w-6"></i> Lampiran
                         </a>
                     </li>
                     <li>
