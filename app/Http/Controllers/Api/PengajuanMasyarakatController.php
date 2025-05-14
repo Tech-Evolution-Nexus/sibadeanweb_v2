1351bb6e9e37a7e15e4d841f5f23200f4fcd7638
Kholzt <99956928+Kholzt@users.noreply.github.com>
2025-05-14 12:35:38 +0700

    Update PengajuanMasyarakatController.php

--- app/Http/Controllers/Api/PengajuanMasyarakatController.php
+++ app/Http/Controllers/Api/PengajuanMasyarakatController.php
@@ -62,7 +62,6 @@ class PengajuanMasyarakatController extends Controller
             [
                 "pengajuanMenunggu" => PengajuanResource::collection($pengajuan),
                 "pengajuanSelesai" => PengajuanResource::collection($pengajuanSelesai),
-                "pengajuan" => $pengajuan
             ]
         );
     }
