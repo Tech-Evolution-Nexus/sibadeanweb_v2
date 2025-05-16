74a4d20a682d9af24c3eca2e8cc9c45d3aeb454e
Kholzt <99956928+Kholzt@users.noreply.github.com>
2025-05-16 12:30:20 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,19 +26,19 @@ export default {
             //     '5xl': ['60px', '72px'],       // text-5xl
             //     '6xl': ['72px', '90px'],       // text-6xl
             //   }
-            fontSize: {
-                'xs': ['10px', '14px'],       // default: 0.75rem (12px)
-                'sm': ['12px', '18px'],       // default: 0.875rem (14px)
-                'base': ['14px', '20px'],     // default: 1rem (16px)
-                'lg': ['16px', '24px'],       // default: 1.125rem (18px)
-                'xl': ['18px', '26px'],       // default: 1.25rem (20px)
-                '2xl': ['20px', '28px'],      // default: 1.5rem (24px)
-                '3xl': ['24px', '32px'],      // default: 1.875rem (30px)
-                '4xl': ['28px', '36px'],      // default: 2.25rem (36px)
-                '5xl': ['32px', '40px'],      // default: 3rem (48px)
-                '6xl': ['36px', '44px'],      // default: 3.75rem (60px)
-                '7xl': ['42px', '50px'],      // default: 4.5rem (72px)
-              }
+            // fontSize: {
+            //     'xs': ['10px', '14px'],       // default: 0.75rem (12px)
+            //     'sm': ['12px', '18px'],       // default: 0.875rem (14px)
+            //     'base': ['14px', '20px'],     // default: 1rem (16px)
+            //     'lg': ['16px', '24px'],       // default: 1.125rem (18px)
+            //     'xl': ['18px', '26px'],       // default: 1.25rem (20px)
+            //     '2xl': ['20px', '28px'],      // default: 1.5rem (24px)
+            //     '3xl': ['24px', '32px'],      // default: 1.875rem (30px)
+            //     '4xl': ['28px', '36px'],      // default: 2.25rem (36px)
+            //     '5xl': ['32px', '40px'],      // default: 3rem (48px)
+            //     '6xl': ['36px', '44px'],      // default: 3.75rem (60px)
+            //     '7xl': ['42px', '50px'],      // default: 4.5rem (72px)
+            //   }
         },
     },
 
