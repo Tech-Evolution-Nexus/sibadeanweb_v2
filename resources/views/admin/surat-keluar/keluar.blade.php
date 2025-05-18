aa2761aeac544a3495de3b62d3d1c2d9c1741f47
Muhammad Nor kholit <99956928+Kholzt@users.noreply.github.com>
2025-05-18 12:26:16 +0700

    update

--- resources/views/admin/surat-keluar/keluar.blade.php
+++ resources/views/admin/surat-keluar/keluar.blade.php
@@ -29,10 +29,10 @@
 
     <x-modal :name="'preview'" :maxWidth="'2xl'" >
         <iframe :src="previewPdf" id="frame" width="100%" height="600px" frameborder="0"></iframe>
-       <button class='px-4 py-2 border bg-white fixed top-5 left-5'
+       <!-- <button class='px-4 py-2 border bg-white fixed top-5 left-5'
                             x-data
                             x-on:click="$dispatch('close-modal', {name: 'preview'}); previewPdf = '{$path}'">
-                            <i class='fa fa-close'></i>
+                            <i class='fa fa-close'></i> -->
         </button>
     </x-modal>
     <x-slot name="script">
