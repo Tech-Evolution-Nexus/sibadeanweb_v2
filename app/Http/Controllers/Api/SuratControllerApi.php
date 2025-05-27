90c406c3e0b480b159438497223168b7bb011115
Ryuushu <kzkzaj@gmail.com>
2025-05-27 20:03:31 +0700

    add surat fix

--- app/Http/Controllers/Api/SuratControllerApi.php
+++ app/Http/Controllers/Api/SuratControllerApi.php
@@ -3,17 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\BeritaResource;
 use App\Http\Resources\SuratResource;
-use App\Models\BeritaModel;
 use Illuminate\Http\Request;
-use App\Models\MasyarakatModel;
-use App\Models\KartuKeluargaModel;
 use App\Models\SuratModel;
-use App\Models\User;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use ResponseHelper;
 
 class SuratControllerApi extends Controller
