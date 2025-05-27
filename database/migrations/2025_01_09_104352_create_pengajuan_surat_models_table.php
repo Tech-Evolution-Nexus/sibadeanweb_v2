2ff4d5b1f95b1f8eac05104044227636d0752946
Ryuushu <kzkzaj@gmail.com>
2025-05-27 07:26:22 +0700

    pengajuan

--- database/migrations/2025_01_09_104352_create_pengajuan_surat_models_table.php
+++ database/migrations/2025_01_09_104352_create_pengajuan_surat_models_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->foreign("nik")->references("nik")->on("masyarakat")->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId("id_surat")->references("id")->on("surat")->restrictOnDelete();
             $table->string("nomor_surat", 50)->nullable();
-            $table->enum("status", ["di_terima_rt", "di_terima_rw", "di_tolak_rt","di_tolak_lurah", "di_tolak_rw", "selesai", "pending", "`dibatalkan`"])->default("pending");
+            $table->string("nik_pemohon", 16);
+            $table->enum("status", ["di_terima_rt", "di_terima_rw", "di_tolak_rt", "di_tolak_lurah", "di_tolak_rw", "selesai", "pending", "`dibatalkan`"])->default("pending");
             $table->string("pengantar_rt")->nullable();
             $table->string("keterangan", 70)->nullable();
             $table->string("keterangan_ditolak", 70)->nullable();
