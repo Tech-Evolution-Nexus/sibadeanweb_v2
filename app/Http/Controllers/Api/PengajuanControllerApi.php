9d99c5b9c0d0953be5c0cc7b17b044c9242143e3
Kholzt <99956928+Kholzt@users.noreply.github.com>
2025-06-15 15:43:34 +0700

    Update PengajuanControllerApi.php

--- app/Http/Controllers/Api/PengajuanControllerApi.php
+++ app/Http/Controllers/Api/PengajuanControllerApi.php
@@ -79,7 +79,7 @@ class PengajuanControllerApi extends Controller
             'id_surat' => $request->id_surat,
             'keterangan' => $request->keterangan,
             'pengantar_rt' => $pengantarPath,
-            'nik_pengaju' => $request->nik_pemohon,
+            'nik_pengaju' => auth()->user()->masyakarat->nik,
             'status' => $status,
         ]);
 
