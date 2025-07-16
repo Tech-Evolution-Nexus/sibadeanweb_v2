3619c7b7e334eb01fe5f692461ea0c2161d7e78c
Kholzt <99956928+Kholzt@users.noreply.github.com>
2025-07-16 14:39:12 +0700

    Update form.blade.php

--- resources/views/admin/pengajuan-surat/form.blade.php
+++ resources/views/admin/pengajuan-surat/form.blade.php
@@ -138,10 +138,10 @@
                                 @foreach ($data->pengajuan->lampiran as $lampiran)
                                     <div class="flex flex-col">
                                         <span class="text-gray-500">{{ $lampiran->nama_lampiran }}</span>
-                                        <img @click="$dispatch('open-modal', {
-                                                                                    name: 'preview'
-                                                                                }),previewImage:'{{ route('private.image') }}?path={{ $lampiran->pivot->gambar }}"
-                                            class="w-full object-contain aspect-video"
+                                        <img x-data x-on:click="
+                                            lampiran = '{{ route('private.image') }}?path={{ $lampiran->pivot->gambar }}';
+                                            $dispatch('open-modal', { name: 'preview', lampiran: lampiran });
+                                        " class="w-full object-contain aspect-video"
                                             src="{{ route('private.image') }}?path={{ $lampiran->pivot->gambar }}" alt="">
                                     </div>
                                 @endforeach
