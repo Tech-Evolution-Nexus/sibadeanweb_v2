84a10698a1790f5253051ff88e1323f406405c12
Kholzt <99956928+Kholzt@users.noreply.github.com>
2025-09-04 13:27:31 +0700

    Update PengajuanController.php

--- app/Http/Controllers/Api/PengajuanController.php
+++ app/Http/Controllers/Api/PengajuanController.php
@@ -199,12 +199,10 @@ class PengajuanController extends Controller
 
 
 
-        foreach ($data->fields as $field) {
-            $value = $this->model->fieldValues
-                ->where("id_field", "=", $field->id)
-                ->where("id_pengajuan", "=", $data->id_pengajuan)
-                ->first();
-            $namaField = "{field_" . strtolower(str_replace(" ", "_", trim($field->nama_field)) . "}");
+        foreach ($data->fieldValues as $field) {
+            $value = $field->value;
+            $namaField = "{" . $field->field->nama_field . "}";
+            // $namaField = "{field_" . strtolower(str_replace(" ", "_", trim($field->nama_field)) . "}");
 
             $html = str_replace($namaField, $value->value ?? "-", $html);
         }
