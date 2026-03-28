2a606612176993a08bea059fee09cbc3aa4538f7
sagita77 <148317761+sagita77@users.noreply.github.com>
2026-03-28 21:14:40 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends("layouts.frontend.front")
 
 @section("content")
+nijcsncjsnjtestt
 <section class=" py-16 relative">
     <img src="{{ asset("assets/image/bg-hero.png") }}"
         class="w-full bg-gray-50 h-full opacity-25 absolute inset-0 object-cover z-[-1]" alt="">
